05a32bfd549b01d9a0f413d147db2bc1edced112
raochongmou <1041060542@qq.com>
2023-05-15 09:35:18 +0800

    react-爱彼迎项目初始化配置(craco-less、craco-config、jsconfig)

---
+++ craco.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require("path");
+const resolves = curname => resolve(__dirname, curname);
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  // less插件
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            // modifyVars: { "@primary-color": "#1DA57A" },
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ],
+  // 别名配置
+  webpack: {
+    alias: {
+      "@": resolves("src"),
+      components: resolves("src/component")
+    }
+  }
+};
